d3199ebb8aefd4c878d0f117da2eae47f00545fc
Saakyous <17621209587@163.com>
2018-08-03 15:05:54 +0800

    定时器

---
+++ TestGo.py
@@ -0,0 +1,8 @@
+
+# import psutil
+# str=psutil.disk_usage(r"\\172.16.119.30\C$")
+# print(str.percent)
+# if str.percent>73.5:
+#     print("大了")
+# else:
+#     print("小了")
---
+++ Timer.py
@@ -0,0 +1,20 @@
+#引入库 threading
+import threading
+
+
+class Person(object):
+    def __init__(self):
+        print("init person")
+
+    def speak(self):
+        print("speak")
+
+
+if __name__ == "__main__":
+    p = Person()
+    while True:
+        timer = threading.Timer(5, Person.speak, (p,))
+        print("start")
+        timer.start()
+        timer.join()
+        print("after join")
