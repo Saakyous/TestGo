bf0f54359f7ab5338b15ced03f1b74eb5dc638d5
Saakyous <17621209587@163.com>
2018-08-03 15:26:49 +0800

    更新

--- TestGo.py
+++ TestGo.py
@@ -1,8 +1,11 @@
 
-# import psutil
-# str=psutil.disk_usage(r"\\172.16.119.30\C$")
-# print(str.percent)
-# if str.percent>73.5:
-#     print("大了")
-# else:
-#     print("小了")
+import psutil
+Host=[r"\\172.16.119.30\C$",r"\\172.16.119.30\D$",r"\\172.16.119.30\E$"]
+for x in Host:
+    str=psutil.disk_usage(x)
+    print(str.percent)
+    if str.percent>73.5:
+        print("大了",x)
+    else:
+        print("小了")
+    # print(x)
